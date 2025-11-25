b6792c5ec060efcd0f2cad351fe6bcc63b45913c
gdaram <gdaram@pictet.com>
2025-11-25 16:17:15 +0100

    feat: update action

--- java-app/src/main/java/com/example/demo/DemoApplication.java
+++ java-app/src/main/java/com/example/demo/DemoApplication.java
@@ -6,7 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
-    public static void main(String[] args) {
+    /**
+     * The main method serves as the entry point for the Spring Boot application.
+     * It launches the application using the SpringApplication class.
+     *
+     * @param args the command-line arguments passed to the application
+     */
+    public static void main(final String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
 
